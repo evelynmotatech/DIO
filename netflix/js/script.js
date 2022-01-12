67852ab559d89f4a3a265cd063db61f54b6053db
evelynmotatech <evelynmota.arq@gmail.com>
2022-01-12 16:22:11 -0300

    pokeflix

---
+++ netflix/js/script.js
@@ -0,0 +1,29 @@
+// $('.owl-carousel').owlCarousel({
+//     center: true,
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
+
+$('.loop').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
